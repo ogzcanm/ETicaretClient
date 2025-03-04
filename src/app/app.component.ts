d95ce8b5e28508bfbce6563aef984780fb7d182c
Ismail Oguzcan Aykar <oaykar53mac@gmail.com>
2025-03-04 04:58:12 +0300

    Angular Bootstrap Jquery Entegrasyonu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+declare var $: any
 
 @Component({
   selector: 'app-root',
@@ -9,3 +10,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ETicaretClient';
 }
+
