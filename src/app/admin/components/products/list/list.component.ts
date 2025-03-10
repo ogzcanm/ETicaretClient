c243350f47be57b52be912ab46d940c6e9576133
Ismail Oguzcan Aykar <oaykar53mac@gmail.com>
2025-03-11 02:35:39 +0300

    Delete Directive Düzeltmeleri

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -30,7 +30,7 @@ export class ListComponent extends BaseComponent implements OnInit{
     this.showSpinner(SpinnerType.BallAtom)
     const allProducts :{totalCount:number,products:List_Product[]} = await this.productService.read(this.paginator? this.paginator.pageIndex: 0,this.paginator? this.paginator.pageSize:5, 
       ()=>this.hideSpinner(SpinnerType.BallAtom),errorMessage=>
-    this.alertifyService.message(errorMessage,{
+      this.alertifyService.message(errorMessage,{
       dismissOthers:true,
       messageType:MessageType.Error,
       position:Position.TopRight
--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -4,6 +4,9 @@ import { BaseComponent, SpinnerType } from '../../base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent, DeleteState } from '../../dialogs/delete-dialog/delete-dialog.component';
+import { HttpClientService } from '../../services/common/http-client.service';
+import { AlertifyService, MessageType, Position } from '../../services/admin/alertify.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 declare var $:any;
 
@@ -16,9 +19,10 @@ export class DeleteDirective{
   constructor(
     private element:ElementRef,
     private _renderer:Renderer2,
-    private productService : ProductService,
+    private httpClientService : HttpClientService,
     private spinner : NgxSpinnerService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private alertifyService:AlertifyService
   ) { 
     const img = _renderer.createElement("img");
     img.setAttribute("src","images/delete.png");
@@ -28,19 +32,36 @@ export class DeleteDirective{
     _renderer.appendChild(element.nativeElement,img)
   }
   @Input() id: string;
+  @Input() controller : string;
   @Output() callback : EventEmitter<any> = new EventEmitter();
   @HostListener("click")//ilgili directivin kullanıldığı dom nesnesi tıklanıldığında
   async onClick(){
     this.openDialog(async ()=>{
       this.spinner.show(SpinnerType.BallAtom);
       const td: HTMLTableCellElement = this.element.nativeElement;
-      await this.productService.delete(this.id)
-      $(td.parentElement).animate({
-        opacity:0,
-        left:"+=50",
-        height:"toogle"
-      },700,()=>{
-        this.callback.emit();
+      this.httpClientService.delete({
+        controller:this.controller
+      },this.id).subscribe(data =>{
+        $(td.parentElement).animate({
+          opacity:0,
+          left:"+=50",
+          height:"toogle"
+        },700,()=>{
+          this.callback.emit();
+          this.alertifyService.message("Ürün Başarıyla Silinmiştir",{
+            dismissOthers:true,
+            messageType:MessageType.Success,
+            position: Position.TopRight,
+          })
+        });
+      },(errorResponse:HttpErrorResponse)=>{
+        this.spinner.hide(SpinnerType.BallAtom)
+        this.alertifyService.message("Ürün Silinirken Beklenmeyen Bir Hata İle Karşılalışmıştır!",{
+          dismissOthers: true,
+          messageType:MessageType.Error,
+          position: Position.TopRight,
+
+        })
       });
     });
   }
