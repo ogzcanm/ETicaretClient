63b7286836b82ac9400877ecf1d5b6f81ca1f4fa
Ismail Oguzcan Aykar <oaykar53mac@gmail.com>
2025-03-26 06:43:45 +0300

    Kullanıcı Kayıt Formu Tasarlama (Reactive Form)

---
+++ src/app/ui/components/register/register.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { User } from '../../../entities/user';
+
+@Component({
+  selector: 'app-register',
+  standalone: false,
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent implements OnInit {
+  constructor(private formBuilder : FormBuilder){
+
+  }
+  frm : FormGroup;
+  ngOnInit(): void {
+    this.frm = this.formBuilder.group({
+      nameSurname: ["", [
+        Validators.required,
+        Validators.maxLength(50),
+        Validators.minLength(3)
+      ]],
+      username: ["", [
+        Validators.required,
+        Validators.maxLength(50),
+        Validators.minLength(3)
+      ]],
+      email: ["", [
+        Validators.required,
+        Validators.maxLength(250),
+        Validators.email
+      ]],
+      password: ["",
+        [
+          Validators.required
+        ]],
+      passwordConfirm: ["",
+        [
+          Validators.required
+        ]]
+    }, {
+      validators: (group: AbstractControl): ValidationErrors | null => {
+        let sifre = group.get("password").value;
+        let sifreTekrar = group.get("passwordConfirm").value;
+        return sifre === sifreTekrar ? null : { notSame: true };
+      }
+    })
+  }
+  get component(){
+    return this.frm.controls;
+  }
+  submitted : boolean=false;
+  onSubmit(data : User){
+    this.submitted = true;
+    if(this.frm.invalid)
+      return;
+  }
+
+}
